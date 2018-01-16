259cfd3073359cd7aa1d06bffb8f54477f64c897
danielrcoura <daniel.coura@ccc.ufcg.edu.br>
2018-01-16 18:29:23 -0300

    interface web

--- ufcg-auxilios.py
+++ ufcg-auxilios.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
-# coding utf-8
+# coding: utf-8
 
 import urllib2
 import string
+import webbrowser
 
 
 def find_edital(request, start_index):
@@ -17,11 +18,67 @@ def find_edital(request, start_index):
         return request[start:end + 2], start, 1
 
 
-url = 'http://www.ufcg.edu.br/prt_ufcg/concursos/concursos.php'
-request = urllib2.urlopen(url).read()
+def count_news(request):
+    arq = open('qtd_editais.txt', 'r')
+    qtd = int(arq.readline())
+    arq.close()
+
+    arq = open('qtd_editais.txt', 'w')
+    cont = request.count('PRAC')
+    arq.write(str(cont))
+    arq.close()
+
+    return cont - qtd
+
+
+HEAD = """
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>UFCG auxílios</title>
+    <meta name="description" content="Source code generated using layoutit.com">
+    <meta name="author" content="LayoutIt!">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="container">
+	<div class="row">
+		<div class="col-md-12">
+"""
+
+FOOT = """
+</table>
+</div>
+</div>
+</div>
+</body>
+"""
+
+URL = 'http://www.ufcg.edu.br/prt_ufcg/concursos/concursos.php'
+
+request = urllib2.urlopen(URL).read().decode('utf-8', 'ignore')
 start_index = string.find(request, '<body>')
 
-cont = 0
+arq = open('index.html', 'w')
+arq.write(HEAD)
+
+cont_editais = count_news(request)
+
+TITLE = """
+<div class="page-header" style="margin-bottom: 30px;">
+	<h1>
+		Editais de auxílio <span style="font-size:20px; color:gray;">
+""" + str(cont_editais) + """ novas atualizações</span>
+	</h1>
+</div>
+"""
+arq.write(TITLE)
+
+arq.write("<table class='table table-hover'>")
 while True:
     edital, start_index, status = find_edital(request, start_index + 1)
 
@@ -29,7 +86,16 @@ while True:
         break
 
     if status != -2:
-        print edital
-        cont += 1
+        if cont_editais != 0:
+            cont_editais -= 1
+            arq.write("<tr class='active'>")
+        else:
+            arq.write("<tr>")
+        arq.write('<td>')
+        arq.write(edital)
+        arq.write('</td>')
 
-print cont
+arq.write(FOOT)
+arq.close()
+
+webbrowser.open('./index.html', autoraise=True)
